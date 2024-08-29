d691e1419b7e43c80050e696f67457ea669a9c29
AniMelendrez <ani.melendrez@gmail.com>
2024-08-29 15:13:56 -0600

    domain and app listening update

--- src/api/create-payment-intent/payment.js
+++ src/api/create-payment-intent/payment.js
@@ -31,4 +31,4 @@ app.get('/session-status', async (req, res) => {
   });
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(3000, () => console.log('Running on port 3000'));
